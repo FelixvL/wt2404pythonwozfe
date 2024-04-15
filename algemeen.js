2bca1ac385ae9710ef52704457ae3126b4a7341a
Lon Lrn <lonlrn@Galaxy-A40-van-Tom.2e.lan>
2024-04-15 22:57:33 +0200

    bootstrap en csv

---
+++ algemeen.js
@@ -0,0 +1,9 @@
+let backendurl = "https://wt2404bewoz.azurewebsites.net"
+// let backendurl = "http://127.0.0.1:5000"
+
+function schrijfmenu(){
+    document.getElementById("menudiv").innerHTML = `
+        <a href="pagina2.html">eerste</a>
+        <a href="pagina3.html">tweede</a>
+    `
+}
